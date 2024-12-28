cecdaa0b0bf28c1544abcf775f960d42a82905dd
Mubbashir Bawany <49834974+MUBBASHIRBAWANY@users.noreply.github.com>
2024-12-28 08:56:14 -0800

    Update ProductControllar.js

--- Controllar/ProductControllar.js
+++ Controllar/ProductControllar.js
@@ -1,60 +1,49 @@
-import PurchaseOrderModel from '../Models/PurchaseOrderModal.js'
+import productModal from "../Models/productModal.js"
 
 
-export const getAllPurchaseOrders = async (req,res) =>{
-    try {
-        const purchaseOrders = await PurchaseOrderModel.find({})
-        res.send(purchaseOrders)
-    } catch (error) {
-        console.log(error)
-    }
+
+export const getAllProducts = (req,res) =>{
+    productModal.find({}).then((product)=>{
+        res.send(product)
+    })
 }
 
-export const postPurchaseOrder = async (req,res) =>{
-    console.log(req.body)
-    try {
-        const purchaseOrder =  await PurchaseOrderModel.create({
-            products : req.body.products,
-            vendor : req.body.vendor,
-            date : req.body.date,
-            purchaseDate: req.body.purchaseDate,
-            Ponumber : req.body.Ponumber
-
-        })
-        
-        res.send(purchaseOrder)
-    } catch (error) {
-        res.send(error)
-    }
-}
-
-
-export const deletePurchaseOrdr = async () =>{
-    // Implement delete logic here
- try{
-    const purchaseOrder = await PurchaseOrderModel.findByIdAndDelete(req.params.id)
-    res.send(purchaseOrder)
- }catch(err){
-    console.log(err)
- }
-}
-
-
-export const updatePurchaseOrder = async (req,res) =>{
+export const postProducts = async (req,res) =>{
     try{
-        const purchaseOrder = await PurchaseOrderModel.findByIdAndUpdate(req.params.id, req.body, {new:true})
-        res.send(purchaseOrder)
+        const newproduct = new productModal(req.body)
+        await productModal.create(newproduct)
+        res.send(newproduct)
+    }catch(err){
+        res.send(err)
+    }
+}
+
+export const Updateproduct = async (req,res) =>{
+    try{
+        const product = await productModal.findByIdAndUpdate(req.params.id, req.body, {new:true})
+        res.send(product)
     }catch(err){
         res.send(err)
     }
 }
 
 
-export const getPurchaseOrderByid = async (req,res) => {
-            try{
-            const product = await PurchaseOrderModel.findById(req.params.id)
-            res.send(product)
-        }catch(err){
-            res.send(err)
-        }
+export const getproductByid = async (req,res) => {
+
+
+    try{
+        const product = await productModal.findById(req.params.id)
+        res.send(product)
+    }catch(err){
+        res.send(err)
     }
+}
+
+export const deleteproduct = async (req,res) =>{
+    try{
+        const product = await productModal.findByIdAndDelete(req.params.id)
+        res.send(product)
+    }catch(err){
+        res.send(err)
+    }
+}
