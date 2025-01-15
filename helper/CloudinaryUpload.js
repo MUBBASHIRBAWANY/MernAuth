98796afbd8197107ca2b413e9db9d9eef4797a20
MUBBASHIRBAWANY <mubbashirbawany@gmail.com>
2025-01-15 12:05:53 -0800

    cloudnary remove

--- helper/CloudinaryUpload.js
+++ helper/CloudinaryUpload.js
@@ -3,34 +3,36 @@ import dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
+
 dotenv.config();
 
-cloudinary.config({
-  cloud_name: process.env.cloudName,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+
+
+
+
+cloudinary.config({ 
+    cloud_name: process.env.cloudName, 
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: async (req, file) => {
-    let folder = 'uploads';
-    let resourceType = 'image';
+    cloudinary: cloudinary,
+    params:(req,file)=> {
+        try{
+            let folder = "uploads/userImage"     
+            return {folder};
+        }catch(err){
+            console.log(err);
+            return {error: "Error in uploading"};
+        }
+        
+      
+    },
+    allowedFormats: ['jpg', 'png', 'jpeg']
+  });
 
-    if (file.mimetype === 'application/pdf') {
-      folder = 'uploads/pdf';
-      resourceType = 'raw'; // Cloudinary uses 'raw' for non-image files like PDFs
-    } else {
-      folder = 'uploads/images';
-    }
-
-    return {
-      folder,
-      resource_type: resourceType,
-      allowed_formats: ['jpg', 'png', 'jpeg', 'pdf'], // Specify allowed formats here
-    };
-  },
-});
 
 const uploads = multer({ storage });
 
